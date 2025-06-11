f9da9f94587216d4f2d2f2b3eb11bcd12319808f
fcormier <felixcormier@gmail.com>
2025-06-11 16:01:26 -0700

    remove wrong env_setup.sh

--- env_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-module load python
-virtualenv --no-download $HOME/jupyter_py3
-source $HOME/jupyter_py3/bin/activate
-mkdir -p ~/.local/share/jupyter/kernels
-pip install --no-index ipykernel
-python -m ipykernel install --user --name hk_ca --display-name "HKCA Python 3.x Kernel"
-pip install --no-index numpy numpy h5py torch matplotlib pandas
